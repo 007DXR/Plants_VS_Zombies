7182970b0729499f6fd7b74033218c7da6d8d6c8
saki-37 <64531306+saki-37@users.noreply.github.com>
2022-05-28 17:00:48 +0800

    Create RepeaterPea.java

---
+++ plant_vs_zombie_simple/src/core/plants/RepeaterPea.java
@@ -0,0 +1,30 @@
+package core.plants;
+
+import java.io.File;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.TreeSet;
+import javax.imageio.ImageIO;
+import core.zombies.Zombie;
+import core.bullets.Bullet;
+
+import core.Constants;
+import core.*;
+
+class RepeaterPea extends Plant{
+    private int shoot_timer = 0; 
+    private List<Bullet> bullet_group; 
+    public RepeaterPea(int x, int y){
+        super(Constants.PLANT_HEALTH, x, y, Constants.RepeaterPea, 1); 
+        this.shoot_timer = 0; 
+    }
+
+    public void attacking(){
+        if (this.current_time - this.shoot_timer > 2000){
+            this.bullet_group.add(Bullet(x, y, y, Constants.BULLET_PEA, Constants.BULLET_DAMAGE_NORMAL, false)); 
+            this.bullet_group.add(Bullet(x + 40, y, y, Constants.BULLET_PEA, Constants.BULLET_DAMAGE_NORMAL, false)); 
+            this.shoot_timer = this.current_time; 
+        }
+    }
+}
