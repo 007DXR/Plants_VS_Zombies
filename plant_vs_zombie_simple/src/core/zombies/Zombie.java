ae40b5ede4b69c1936ae8cbe6cfe9bd508dc72f5
DXR <1900013018@pku.edu.cn>
2022-05-28 18:31:04 +0800

    delete unused docs

--- plant_vs_zombie_simple/src/core/zombies/Zombie.java
+++ plant_vs_zombie_simple/src/core/zombies/Zombie.java
@@ -1,4 +1,5 @@
 package core.zombies;
+
 import java.io.File;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -9,7 +10,6 @@ import javax.imageio.ImageIO;
 import core.*;
 import core.plants.Plant;
 import core.Constants;
-
 
 // public class Demo {
 //     public static void main(String args[]) throws IOException {
@@ -26,35 +26,34 @@ import core.Constants;
 // }
 
 public abstract class Zombie {
-   
+
     // 大小
     public int width;
     public int height;
     public String old_state;
     public String name;
-    
 
-    public int frame_index=0;
+    public int frame_index = 0;
     public int frame_num;
 
     public BufferedImage image;
     public BufferedImage ice_trap_image;
-// 位置----------------------------------------
-public int x;
-public int y;
-public int centerx;
-public int bottom;
-public int ice_centerx;
-public int ice_bottom;
-   
-// ----------------------------------------
+    // 位置----------------------------------------
+    public int x;
+    public int y;
+    public int centerx;
+    public int bottom;
+    public int ice_centerx;
+    public int ice_bottom;
+
+    // ----------------------------------------
     int health;
     int damage;
-    boolean dead=false;
-    boolean losHead=false;
-    boolean helmet=false;
+    boolean dead = false;
+    boolean losHead = false;
+    boolean helmet = false;
     // List<ZombieHead> head_group;
-// 时间----------------------------------------
+    // 时间----------------------------------------
     long walk_timer = 0;
     long animate_timer = 0;
     long attack_timer = 0;
@@ -62,21 +61,21 @@ public int ice_bottom;
     long hit_timer = 0;
     long freeze_timer = 0;
     long current_time;
-// ----------------------------------------
-    String state=Constants.WALK;
+    // ----------------------------------------
+    String state = Constants.WALK;
     int animate_interval = 150;
     int ice_slow_ratio = 1;
 
     double speed = 1;
     boolean is_hypno = false;
-    boolean prey_is_plant=true;
+    boolean prey_is_plant = true;
     Plant prey;
 
-   //tool类 -----------------------------------
+    // tool类 -----------------------------------
     // static JSONObject toolZombieRect;
     // static HashMap GFX=new HashMap<String,ArrayList<BufferedImage>>();
-   // -----------------------------------    
-   public ArrayList<BufferedImage> frames;
+    // -----------------------------------
+    public ArrayList<BufferedImage> frames;
     ArrayList<BufferedImage> helmet_attack_frames;
     ArrayList<BufferedImage> die_frames;
     ArrayList<BufferedImage> walk_frames;
@@ -91,7 +90,6 @@ public int ice_bottom;
     // String losthead_attack_name;
     // String die_name;
     // String boomdie_name;
-    
 
     // this.boomdie_frames = []
     public Zombie(int x, int y, String name, int health, int damage) {
@@ -101,8 +99,9 @@ public int ice_bottom;
         // this.load_all_gfx();
         this.loadImages();
         this.frame_num = this.frames.size();
-        
-        // this.loadFrames(this.die_frames, this.die_name, toolZombieRect.getJSONObject(name).getInt("x"),Constants.BLACK);
+
+        // this.loadFrames(this.die_frames, this.die_name,
+        // toolZombieRect.getJSONObject(name).getInt("x"),Constants.BLACK);
 
         this.image = this.frames.get(this.frame_index);
         // this.rect = this.image.get_rect();
@@ -111,47 +110,56 @@ public int ice_bottom;
 
         this.health = health;
         this.damage = damage;
-        
+
     }
-    
-    
-     // 判断鼠标点击
-     public boolean checkMouseClick(int x_, int y_) {
+
+    // 判断鼠标点击
+    public boolean checkMouseClick(int x_, int y_) {
         if (x_ >= x && x_ <= (x + width) && y_ >= y && y_ <= (y + height))
             return true;
         else
             return false;
     }
+
     public abstract void loadImages();
 
-   
     // the zombie is hypo&&attack other zombies when it ate a HypnoShroom
 
-    public void loadFrames(ArrayList<BufferedImage>frames,String name,int image_x,Color colorkey)
-    {   
-        TreeSet<Tool.Img> frame_list=(TreeSet<Tool.Img>) Tool.GFX.get(name);
+    public void loadFrames(ArrayList<BufferedImage> frames, String name, int image_x, Color colorkey) {
+        TreeSet<Tool.Img> frame_list = (TreeSet<Tool.Img>) Tool.GFX.get(name);
         for (Tool.Img frame : frame_list) {
             BufferedImage rect = frame.image;
             int width = rect.getWidth();
-            int height=rect.getHeight();
+            int height = rect.getHeight();
             width -= image_x;
+            System.out.print(width);
+            System.out.println(frames);
+            if (frames == null)
+                System.out.print("empty");
             frames.add(frame.image.getSubimage(image_x, 0, width, height));
-                // tool.get_image(frame, image_x, 0, width, height, colorkey));
+            // tool.get_image(frame, image_x, 0, width, height, colorkey));
         }
     }
-    public void update( ) {
+
+    public void update() {
         // this.current_time = game_info[Constants.CURRENT_TIME];
-        this.current_time=System.currentTimeMillis();
+        this.current_time = System.currentTimeMillis();
         this.handleState();
         this.updateIceSlow();
         this.animation();
     }
+
     public void handleState() {
-        if (this.state==Constants.WALK)this.walking();
-        if (this.state==Constants.ATTACK)this.attacking();
-        if (this.state==Constants.DIE)this.dying();
-        if (this.state==Constants.FREEZE)this.freezing();
+        if (this.state == Constants.WALK)
+            this.walking();
+        if (this.state == Constants.ATTACK)
+            this.attacking();
+        if (this.state == Constants.DIE)
+            this.dying();
+        if (this.state == Constants.FREEZE)
+            this.freezing();
     }
+
     public void walking() {
         if (this.health <= 0) {
             this.setDie();
@@ -166,12 +174,11 @@ public int ice_bottom;
             }
         }
         if (this.current_time - this.walk_timer > Constants.ZOMBIE_WALK_INTERVAL *
-        this.getTimeRatio()) {
+                this.getTimeRatio()) {
             this.walk_timer = this.current_time;
-            if( this.is_hypno) {
+            if (this.is_hypno) {
                 this.x += this.speed;
-            }
-            else {
+            } else {
                 this.x -= this.speed;
             }
         }
@@ -191,9 +198,9 @@ public int ice_bottom;
             if (this.prey.health > 0) {
                 if (this.prey_is_plant) {
                     this.prey.setDamage(this.damage, this);
-                } 
+                }
                 // else {
-                //     this.prey.setDamage(this.damage);
+                // this.prey.setDamage(this.damage);
                 // }
             }
             this.attack_timer = this.current_time;
@@ -207,15 +214,14 @@ public int ice_bottom;
     public void dying() {
 
     }
+
     public void freezing() {
-        if (this.health <= 0 ){
-          this.setDie();
-        }
-        else if( this.health <= Constants.LOSTHEAD_HEALTH &&  this.losHead==false) {
+        if (this.health <= 0) {
+            this.setDie();
+        } else if (this.health <= Constants.LOSTHEAD_HEALTH && this.losHead == false) {
             if (this.old_state == Constants.WALK) {
                 this.changeFrames(this.losthead_walk_frames);
-            }
-            else {
+            } else {
                 this.changeFrames(this.losthead_attack_frames);
             }
             this.setLostHead();
@@ -224,20 +230,23 @@ public int ice_bottom;
             this.setWalk();
         }
     }
-    public void setLostHead(){
+
+    public void setLostHead() {
         this.losHead = true;
         // if (this.head_group.size()!=0) {
-        //     this.head_group.add(new ZombieHead(this.centerx, this.bottom));
+        // this.head_group.add(new ZombieHead(this.centerx, this.bottom));
         // }
     }
+
     public void changeFrames(ArrayList<BufferedImage> frames) {
         // '''change image frames&&modify rect position'''
         this.frames = frames;
         this.frame_num = this.frames.size();
         this.frame_index = 0;
         this.image = this.frames.get(this.frame_index);
-       
+
     }
+
     // 调整亮度
     public BufferedImage adjustBrightness(BufferedImage image_, int alpha) {
         int width = image_.getWidth();
@@ -256,17 +265,18 @@ public int ice_bottom;
         }
         return output;
     }
+
     public void animation() {
         if (this.state == Constants.FREEZE) {
 
-            this.adjustBrightness(this.image,192);
+            this.adjustBrightness(this.image, 192);
             return;
         }
         if (this.current_time - this.animate_timer > this.animate_interval *
-        this.getTimeRatio()) {
+                this.getTimeRatio()) {
             this.frame_index += 1;
             if (this.frame_index >= this.frame_num) {
-                if (this.state == Constants.DIE ){
+                if (this.state == Constants.DIE) {
                     // this.kill();
                     return;
                 }
@@ -275,18 +285,18 @@ public int ice_bottom;
             this.animate_timer = this.current_time;
         }
         this.image = this.frames.get(this.frame_index);
-        if (this.is_hypno ){
+        if (this.is_hypno) {
             // this.image = pg.transform.flip(this.image, true, false)
         }
-        if(this.current_time - this.hit_timer >= 200){
+        if (this.current_time - this.hit_timer >= 200) {
 
-            this.adjustBrightness(this.image,255);
-        }
-        else {
+            this.adjustBrightness(this.image, 255);
+        } else {
 
-            this.adjustBrightness(this.image,192);
+            this.adjustBrightness(this.image, 192);
         }
     }
+
     public int getTimeRatio() {
         return this.ice_slow_ratio;
     }
@@ -319,29 +329,29 @@ public int ice_bottom;
 
         // if (this.helmet) {
         // this.changeFrames(this.helmet_walk_frames);
-        // } else 
+        // } else
         if (this.losHead) {
-        this.changeFrames(this.losthead_walk_frames);
+            this.changeFrames(this.losthead_walk_frames);
 
         } else {
-        this.changeFrames(this.walk_frames);
+            this.changeFrames(this.walk_frames);
         }
     }
     // public void setAttack(Plant prey,Boolean is_plant) {
-    //     this.prey = prey;
-    //     // # prey can be plant||other zombies
-    //     this.prey_is_plant = is_plant;
-    //     this.state = Constants.ATTACK;
-    //     this.attack_timer = this.current_time;
-    //     this.animate_interval = 100
+    // this.prey = prey;
+    // // # prey can be plant||other zombies
+    // this.prey_is_plant = is_plant;
+    // this.state = Constants.ATTACK;
+    // this.attack_timer = this.current_time;
+    // this.animate_interval = 100
 
-    //     if (this.helmet) {
-    //         this.changeFrames(this.helmet_attack_frames);
-    //     }else if (this.losHead) {
-    //         this.changeFrames(this.losthead_attack_frames);
-    //     }else {
-    //         this.changeFrames(this.attack_frames);
-    //     }
+    // if (this.helmet) {
+    // this.changeFrames(this.helmet_attack_frames);
+    // }else if (this.losHead) {
+    // this.changeFrames(this.losthead_attack_frames);
+    // }else {
+    // this.changeFrames(this.attack_frames);
+    // }
     // }
     public void setDie() {
         this.state = Constants.DIE;
@@ -363,14 +373,14 @@ public int ice_bottom;
         // this.ice_trap_rect = ice_trap_image.get_rect()
         this.ice_centerx = this.centerx;
         this.ice_bottom = this.bottom;
-   
+
     }
 
     // public void drawFreezeTrap() {
-    //     if (this.state == Constants.FREEZE) {
-            
-    //     surface.blit(this.ice_trap_image, this.ice_trap_rect);
-    //     }
+    // if (this.state == Constants.FREEZE) {
+
+    // surface.blit(this.ice_trap_image, this.ice_trap_rect);
+    // }
     // }
     public void setHypno() {
         this.is_hypno = true;
