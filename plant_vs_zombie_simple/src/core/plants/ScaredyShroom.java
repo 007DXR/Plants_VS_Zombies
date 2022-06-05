06019a75414e2365d5530dc82c652de2a2e1901a
saki-37 <64531306+saki-37@users.noreply.github.com>
2022-06-05 12:45:00 +0800

    white

--- plant_vs_zombie_simple/src/core/plants/ScaredyShroom.java
+++ plant_vs_zombie_simple/src/core/plants/ScaredyShroom.java
@@ -58,9 +58,9 @@ public class  ScaredyShroom extends Plant{
     }
 
     public void loadImages(String name, double scale){
-        loadFrames(this.frames, name, Constants.BLACK);
-        loadFrames(this.cry_frames, name+"Cry", Constants.BLACK);
-        loadFrames(this.sleep_frames, name+"Sleep", Constants.BLACK);
+        loadFrames(this.frames, name, Constants.WHITE);
+        loadFrames(this.cry_frames, name+"Cry", Constants.WHITE);
+        loadFrames(this.sleep_frames, name+"Sleep", Constants.WHITE);
     }
 
 }
