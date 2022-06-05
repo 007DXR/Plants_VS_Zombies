7d42bbc5744c367b4ae6cebccfffbe9b769e179e
saki-37 <64531306+saki-37@users.noreply.github.com>
2022-06-05 12:53:59 +0800

    add scale

--- plant_vs_zombie_simple/src/core/plants/PeaShooter.java
+++ plant_vs_zombie_simple/src/core/plants/PeaShooter.java
@@ -32,6 +32,6 @@ public class PeaShooter extends Plant{
     }
 
     public void loadImages(String name, double scale){
-        loadFrames(this.frames, name, Constants.BLACK);
+        loadFrames(this.frames, name, Constants.BLACK, scale);
     }
 }
